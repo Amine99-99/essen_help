af050c7aa9df73d8adf44e6c34cfdaca55c37c9f
Amine Mansouri <mansouriamine77@gmail.com>
2025-10-03 17:23:21 +0200

    update

--- src/app/responsive/mobile/mobile_menu.jsx
+++ src/app/responsive/mobile/mobile_menu.jsx
@@ -13,14 +13,16 @@ const MobileMenu=()=>{
         
                 
 
-                <main className='flex flex-col w-[100vw]  z-10 rounded h-[80vh] bg-white  justify-center items-center gap-10 font-bold text-gray-800 text-[20px] menu'>
+                <main className='flex flex-col w-[100vw]  z-10 rounded h-[80vh] bg-white  justify-start pt-3 items-center gap-5 font-bold text-gray-800 text-[20px] menu'>
+                   <div className='flex flex-col justify-start items-start gap-2'>
                     <Link onClick={toggleMenu} className='' href='/'>Home</Link>
                     <Link onClick={toggleMenu}  href='/service'>Our Services</Link>
                     <Link onClick={toggleMenu}  href='/our_team'>Our Team</Link>
                     <Link  onClick={toggleMenu} href='/help'>Get Help</Link>
                     <Link onClick={toggleMenu}  href='/faq'>FAQ</Link>
-                    <Link onClick={toggleMenu}  href='/feed_back'>Feedback</Link>
-                    <Link onClick={toggleMenu}  href='/contact'>Contact Us</Link>
+                  
+                    <Link onClick={toggleMenu}  href='/feed_back'>Contact Us</Link>
+                    </div>
                 
         </main>
     )
