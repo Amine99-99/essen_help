03d48276dee11232beec02eca4d98741b8ba375d
Amine Mansouri <mansouriamine77@gmail.com>
2025-10-05 17:23:14 +0200

    update

--- src/app/home/footer.jsx
+++ src/app/home/footer.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import ToggleLng from './toggleLng'
+import { useTranslation } from 'react-i18next';
 
 
 
 
 const Footer=()=>{
+  const {t} = useTranslation('header')
     return(
        
         <footer className='p-5 bg-gray-100 font-semibold text-[16px] '>
@@ -29,13 +31,13 @@ const Footer=()=>{
                   <div className='w-[60vw] '>
         
                         <div className=' flex flex-col gap-1 text-[16px] justify-start p-5 items-start  xl:grid  grid-cols-3 flex justify-start items-start  text-gray-800 '>
-                            <Link className='' href='/'>Home</Link>
-                            <Link href='/service'>Our Services</Link>
-                            <Link href='/our_team'>Our Team</Link>
-                            <Link href='/help'>Get Help</Link>
-                            <Link href='/faq'>FAQ</Link>
+                            <Link className='' href='/'>{t('home')}</Link>
+                            <Link href='/service'>{t('services')}</Link>
+                            <Link href='/our_team'>{t('team')}</Link>
+                            <Link href='/help'>{t('help')}</Link>
+                            <Link href='/faq'>{t('FAQ')}</Link>
                         
-                            <Link href='/feed_back'>Contact Us</Link>
+                            <Link href='/feed_back'>{t('contact')}</Link>
                         </div>
                     </div>
                      <div><ToggleLng/></div>
@@ -43,7 +45,7 @@ const Footer=()=>{
                    
                      
                 
-                 <p className="text-sm text-gray-600">© 2025 Help In Essen. All rights reserved.</p>
+                 <p className="text-sm text-gray-600">{t('right')}</p>
                  </footer>
                
 
